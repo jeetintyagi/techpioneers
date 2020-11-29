1d51ceaf73eaff50357daf3709c7db46f072bd3a
jeetintyagi <jt9999709701@gmail.com>
2020-11-29 20:20:49 +0530

    📦 NEW: scoll btn with animation

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Slider from '../Slider/Slider';
 import { ScrollButton } from '../ScrollButton';
+import { motion } from 'framer-motion';
 import {
   HeroContainer,
   HeroBg,
@@ -13,6 +14,7 @@ import {
   ArrowForward,
   ArrowRight,
 } from './HeroElements';
+import { BsChevronDoubleDown } from 'react-icons/bs';
 
 const HeroSection = () => {
   const [hover, setHover] = useState(false);
@@ -40,7 +42,7 @@ const HeroSection = () => {
             Computer Science Society Aryabhatta College | University of Delhi{' '}
           </HeroP>
 
-          <HeroBtnWrapper>
+          {/* <HeroBtnWrapper>
             <ScrollButton
               to='events'
               onMouseEnter={onHover}
@@ -54,6 +56,39 @@ const HeroSection = () => {
               duration={500}
             >
               Get started {hover ? <ArrowForward /> : <ArrowRight />}
+            </ScrollButton>
+          </HeroBtnWrapper> */}
+          <HeroBtnWrapper>
+            <ScrollButton
+              style={{
+                background: 'transparent',
+                fontSize: '50px',
+                color: 'rgb(248,98,82)',
+              }}
+              to='about'
+              bigPad='true'
+              fontBig='true'
+              dark='true'
+              spy={true}
+              smooth={true}
+              offset={-80}
+              duration={700}
+            >
+              <motion.i
+                initial={{ y: -10, opacity: 2, scale: 1 }}
+                animate={{ y: 20, opacity: 0.2, scale: 0.6 }}
+                whileHover={{ opacity: 1 }}
+                transition={{
+                  type: 'tween',
+                  repeat: Infinity,
+                  duration: 2,
+                  repeatType: 'reverse',
+                  stiffness: 300,
+                  damping: 30,
+                }}
+              >
+                <BsChevronDoubleDown />
+              </motion.i>
             </ScrollButton>
           </HeroBtnWrapper>
         </HeroContent>
