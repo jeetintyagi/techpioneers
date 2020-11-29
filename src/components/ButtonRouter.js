185d722547e7af303d043f44f788c80f9e05adb4
jeetintyagi <jt9999709701@gmail.com>
2020-11-29 18:53:55 +0530

    👌 IMPROVE: color of buttons using linear-gradient

--- src/components/ButtonRouter.js
+++ src/components/ButtonRouter.js
@@ -8,7 +8,14 @@ import { Link } from 'react-router-dom';
 
 export const ButtonRouter = styled(Link)`
   border-radius: 50px;
-  background: ${({ primary }) => (primary ? '#01BF71' : '#F86252')};
+  background: ${({ primary }) =>
+    primary
+      ? '#01BF71'
+      : `linear-gradient(
+    90deg,
+    rgba(248, 98, 82, 1) 40%,
+    rgba(252, 176, 69, 1) 100%
+  )`};
   white-space: nowrap;
   padding: ${({ bigPad }) => (bigPad ? '14px 48px' : '12px 30px')};
   font-size: ${({ fontBig }) => (fontBig ? '20px' : '16px')};
@@ -26,7 +33,15 @@ export const ButtonRouter = styled(Link)`
 
   &:hover {
     background: ${({ dark, primary }) =>
-      primary ? '#fff' : dark ? '#F86252' : '#fff'};
+      primary
+        ? '#fff'
+        : dark
+        ? `linear-gradient(
+    90deg,
+    rgba(248, 98, 82, 1) 40%,
+    rgba(252, 176, 69, 1) 100%
+  );`
+        : '#fff'};
     color: ${({ dark, primary }) =>
       primary ? '#010606' : dark ? '#fff' : '#010606'};
   }
--- src/components/ScrollButton.js
+++ src/components/ScrollButton.js
@@ -3,7 +3,14 @@ import { Link as LinkScroll } from 'react-scroll';
 
 export const ScrollButton = styled(LinkScroll)`
   border-radius: 50px;
-  background: ${({ primary }) => (primary ? '#01BF71' : '#F86252')};
+  background: ${({ primary }) =>
+    primary
+      ? '#01BF71'
+      : `linear-gradient(
+    90deg,
+    rgba(248, 98, 82, 1) 40%,
+    rgba(252, 176, 69, 1) 100%
+  )`};
   white-space: nowrap;
   padding: ${({ bigPad }) => (bigPad ? '14px 48px' : '12px 30px')};
   font-size: ${({ fontBig }) => (fontBig ? '20px' : '16px')};
@@ -20,7 +27,15 @@ export const ScrollButton = styled(LinkScroll)`
 
   &:hover {
     background: ${({ dark, primary }) =>
-      primary ? '#fff' : dark ? '#F86252' : '#fff'};
+      primary
+        ? '#fff'
+        : dark
+        ? `linear-gradient(
+    90deg,
+    rgba(248, 98, 82, 1) 40%,
+    rgba(252, 176, 69, 1) 100%
+  )`
+        : '#fff'};
     color: ${({ dark, primary }) =>
       primary ? '#010606' : dark ? '#fff' : '#010606'};
   }
