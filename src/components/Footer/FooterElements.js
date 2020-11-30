782e6abe5270bc8ab4e31f63d3f068c5ffbd86e3
jeetintyagi <jt9999709701@gmail.com>
2020-11-30 17:02:24 +0530

    👌 IMPROVE: Footer style and content

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -42,7 +42,7 @@ export const FooterLinkItems = styled.div`
   text-align: left;
   width: 160px;
   box-sizing: border-box;
-  color: #010606;
+  color: #fbfbfb;
 
   @media screen and (max-width: 420px) {
     margin: 0;
@@ -52,18 +52,84 @@ export const FooterLinkItems = styled.div`
 `;
 
 export const FooterLinkTitle = styled.h1`
-  font-size: 16px;
+  font-size: 20px;
   margin-bottom: 16px;
 `;
 
 export const FooterLink = styled(Link)`
-  color: #010606;
+  color: #fbfbfb;
   text-decoration: none;
   margin-bottom: 0.5rem;
-  font-size: 14px;
+  font-size: 16px;
 
   &:hover {
-    color: #fbfbfb;
+    color: #010606;
+    font-weight: 700;
     transition: 0.3s ease-in-out;
   }
+`;
+
+export const SocialMedia = styled.section`
+  max-width: 1000px;
+  width: 100%;
+`;
+
+export const SocialMediaWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1100px;
+  margin: 40px auto 0 auto;
+
+  @media screen and (max-width: 820px) {
+    flex-direction: column;
+  }
+`;
+
+export const SocialLogo = styled(Link)`
+  color: #fff;
+  justify-self: start;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 1.5rem;
+  align-items: center;
+  margin-bottom: 16px;
+  font-weight: bold;
+`;
+
+export const WebsiteRights = styled.small`
+  color: #fff;
+  margin-bottom: 16px;
+`;
+
+export const SocialIcons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 240px;
+`;
+
+export const SocialIconLink = styled.a`
+  color: #fff;
+  font-size: 24px;
+
+  &:hover {
+    color: #010606;
+    transition: 0.3s ease-in-out;
+  }
+`;
+
+export const ImgWrap = styled.div`
+  max-width: 80px;
+  height: 100%;
+`;
+
+export const Img = styled.img`
+  width: 100%;
+  margin: 0 0 10px 0;
+  padding: 1px;
+  overflow: hidden;
+  -o-object-fit: scale-down;
+  object-fit: scale-down;
+  object-fit: cover;
 `;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
-
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaGithub,
+  FaLinkedin,
+  FaUniversity,
+  FaBuilding,
+  FaGlobe,
+} from 'react-icons/fa';
+import TechPioneers from '../../images/Techpioneers.png';
 import {
   FooterContainer,
   FooterWrap,
@@ -8,6 +18,14 @@ import {
   FooterLinkItems,
   FooterLinkTitle,
   FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcons,
+  SocialIconLink,
+  WebsiteRights,
+  ImgWrap,
+  Img,
 } from './FooterElements';
 
 const Footer = () => {
@@ -19,47 +37,91 @@ const Footer = () => {
             <FooterLinkItems>
               <FooterLinkTitle>About Us</FooterLinkTitle>
 
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/events'>Events</FooterLink>
+              <FooterLink to='/annualreport'>Annual Report</FooterLink>
+              <FooterLink to='/team'>Team</FooterLink>
+              <FooterLink to='/blogs'>Blogs</FooterLink>
+              <FooterLink to='/celestech'>Celestech</FooterLink>
             </FooterLinkItems>
             <FooterLinkItems>
-              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLinkTitle>Contact Us</FooterLinkTitle>
 
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>Contact</FooterLink>
+              <FooterLink to='/blogs'>Sponsor us</FooterLink>
             </FooterLinkItems>
           </FooterLinksWrapper>
           <FooterLinksWrapper>
             <FooterLinkItems>
-              <FooterLinkTitle>About Us</FooterLinkTitle>
-
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
-            </FooterLinkItems>
-            <FooterLinkItems>
-              <FooterLinkTitle>About Us</FooterLinkTitle>
-
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
-              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLinkTitle>University </FooterLinkTitle>
+              <SocialIconLink href='http://www.du.ac.in/du/' target='_blank'>
+                {' '}
+                <FaUniversity />
+              </SocialIconLink>
+              <SocialIconLink
+                href='https://aryabhattacollege.ac.in/'
+                target='_blank'
+              >
+                {' '}
+                <FaBuilding />
+              </SocialIconLink>
+              <SocialIconLink href='https://celestech.live/' target='_blank'>
+                {' '}
+                <FaGlobe />
+              </SocialIconLink>
             </FooterLinkItems>
           </FooterLinksWrapper>
         </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <ImgWrap>
+              <Img src={TechPioneers} alt='TechPioneers' />
+            </ImgWrap>
+            <SocialLogo to='/'>TechPioneers</SocialLogo>
+            <WebsiteRights>
+              TechPioneers © {new Date().getFullYear()} <br />
+              All Rights Reserved
+            </WebsiteRights>
+            <br />
+            <SocialIcons>
+              <SocialIconLink
+                href='https://www.facebook.com/techpioneersofficial'
+                target='_blank'
+                aria-label='Facebook'
+              >
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink
+                href='https://www.instagram.com/techpioneersofficial/'
+                target='_blank'
+                aria-label='Instagram'
+              >
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink
+                href='https://twitter.com/techpioneers01'
+                target='_blank'
+                aria-label='Twitter'
+              >
+                <FaTwitter />
+              </SocialIconLink>
+
+              <SocialIconLink
+                href='https://www.linkedin.com/company/techpioneersofficial/'
+                target='_blank'
+                aria-label='Linkedin'
+              >
+                <FaLinkedin />
+              </SocialIconLink>
+              <SocialIconLink
+                href='https://github.com/jeetintyagi/techpioneers'
+                target='_blank'
+                aria-label='Github'
+              >
+                <FaGithub />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
       </FooterWrap>{' '}
     </FooterContainer>
   );
