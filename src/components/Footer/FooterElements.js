007db646890c994d841a2a67b48a457ca73c3fca
jeetintyagi <jt9999709701@gmail.com>
2020-11-30 16:00:08 +0530

    📦 NEW: footer component with responsiveness

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -1,14 +1,69 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-export const FooterContainer = styled.div`
+export const FooterContainer = styled.footer`
+  background: rgb(248, 98, 91);
+  background: linear-gradient(
+    90deg,
+    rgba(248, 98, 82, 1) 40%,
+    rgba(252, 176, 69, 1) 100%
+  );
+`;
+
+export const FooterWrap = styled.div`
+  padding: 40px 24px;
   display: flex;
-  background: #f9f9f9;
-  color: #010606;
-  padding: 150px 100px;
-  height: 500px;
-  z-index: 1;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
-  position: sticky; // !mportant
-  position: -webkit-sticky; /* Safari */
+  max-width: 1100px;
+  margin: 0 auto;
+`;
+
+export const FooterLinksContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  @media screen and (max-width: 820px) {
+    padding-top: 32px;
+  }
+`;
+
+export const FooterLinksWrapper = styled.div`
+  display: flex;
+  @media screen and (max-width: 820px) {
+    flex-direction: column;
+  }
+`;
+
+export const FooterLinkItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 16px;
+  text-align: left;
+  width: 160px;
+  box-sizing: border-box;
+  color: #010606;
+
+  @media screen and (max-width: 420px) {
+    margin: 0;
+    padding: 10px;
+    width: 100%;
+  }
+`;
+
+export const FooterLinkTitle = styled.h1`
+  font-size: 16px;
+  margin-bottom: 16px;
+`;
+
+export const FooterLink = styled(Link)`
+  color: #010606;
+  text-decoration: none;
+  margin-bottom: 0.5rem;
+  font-size: 14px;
+
+  &:hover {
+    color: #fbfbfb;
+    transition: 0.3s ease-in-out;
+  }
 `;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,68 @@
 import React from 'react';
-import { FooterContainer } from './FooterElements';
+
+import {
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+} from './FooterElements';
 
 const Footer = () => {
-  return <FooterContainer>Footer</FooterContainer>;
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+              <FooterLink to='/blogs'>lorem</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+      </FooterWrap>{' '}
+    </FooterContainer>
+  );
 };
 
 export default Footer;
