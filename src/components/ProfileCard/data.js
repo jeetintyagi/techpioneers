d71072e5794c39f20c3b40e82e0b24f9ce1f8ccb
jeetintyagi <jt9999709701@gmail.com>
2020-12-09 14:56:57 +0530

    📦 NEW: data for ProfileCards

---
+++ src/components/ProfileCard/data.js
@@ -0,0 +1,37 @@
+import AvatarImg from '../../images/Avatar.png';
+const prefix = 'mailto:';
+
+export const MentorDetails = [
+  {
+    id: 1,
+    to: 'https://www.linkedin.com/in/jitin-tyagi-b0a915195/',
+    ImgSrc: AvatarImg,
+    caption: 'Jitin Tyagi',
+    linkedin: 'https://github.com/jeetintyagi/techpioneers',
+    email: prefix + 'jt9999709701@gmail.com',
+  },
+  {
+    id: 2,
+    to: 'https://www.linkedin.com/in/jitin-tyagi-b0a915195/',
+    ImgSrc: AvatarImg,
+    caption: 'Jitin',
+    linkedin: 'https://github.com/jeetintyagi/techpioneers',
+    email: prefix + 'jt9999709701@gmail.com',
+  },
+  {
+    id: 3,
+    to: 'https://www.linkedin.com/in/jitin-tyagi-b0a915195/',
+    ImgSrc: AvatarImg,
+    caption: 'Jitin',
+    linkedin: 'https://github.com/jeetintyagi/techpioneers',
+    email: prefix + 'jt9999709701@gmail.com',
+  },
+  {
+    id: 4,
+    to: 'https://www.linkedin.com/in/jitin-tyagi-b0a915195/',
+    ImgSrc: AvatarImg,
+    caption: 'Jitin',
+    linkedin: 'https://github.com/jeetintyagi/techpioneers',
+    email: prefix + 'jt9999709701@gmail.com',
+  },
+];
